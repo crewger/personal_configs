428288f1785637e63bf9a72814a7980eee66c9a8
K.A <thecrewger@ya.ru>
2018-01-09 17:56:18 +0300

    moved home configs to ./home

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-home_dir='$( getent passwd "$USER" | cut -d: -f6 )'
-echo $home_dir
+home_dir=$( getent passwd "$USER" | cut -d: -f6 )
+repo_dir=`dirname $0`
 
+# Files to ~/
+
+cp $repo_dir/home/tmux.conf $home_dir/.tmux.conf
